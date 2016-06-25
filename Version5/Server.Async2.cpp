ca8a2d92398695fd12df7b829011da6d436fe8ab
Loki Astari <Loki.Astari@gmail.com>
2016-06-25 13:46:58 -0700

    Added an alternative version of Async

---
+++ Version5/Server.Async2.cpp
@@ -0,0 +1,52 @@
+
+#include "Socket.h"
+#include "ProtocolHTTP.h"
+#include "Common.h"
+#include <list>
+#include <iostream>
+#include <future>
+#include <mutex>
+#include <condition_variable>
+
+namespace Sock = ThorsAnvil::Socket;
+
+class FutureQueue
+{
+    using MyFuture   = std::future<void>;
+    using FutureList = std::list<MyFuture>;
+
+    FutureList                  futures;
+    std::mutex                  mutex;
+
+    public:
+        template<typename T>
+        void addFuture(T&& lambda)
+        {
+            std::unique_lock<std::mutex> lock(mutex);
+            FutureList::iterator future = futures.insert(futures.end(), std::future<void>());
+            (*future)    = std::async(std::launch::async, [this, future, lambda = std::move(lambda)]() mutable
+                                        {
+                                            lambda();
+                                            (*future).wait();
+                                            std::unique_lock<std::mutex> lock(this->mutex);
+                                            this->futures.erase(future);
+                                        }
+                                     );
+        }
+};
+
+int main(int argc, char* argv[])
+{
+    std::string         data     = Sock::commonSetUp(argc, argv);
+    int                 finished = 0;
+    Sock::ServerSocket  server(8085);
+    FutureQueue         future;
+
+
+    while(!finished)
+    {
+        Sock::DataSocket  accept  = server.accept();
+
+        future.addFuture([accept = std::move(accept), &server, &data, &finished]() mutable {Sock::worker(std::move(accept), server, data, finished);});
+    }
+}
